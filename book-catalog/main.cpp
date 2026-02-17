9c5aa24005f518adc7dd6df4a97ba8bfd8453779
Vika <vkmoiseienko@gmail.com>
2026-02-17 21:35:53 +0000

    Upd main.cpp

--- book-catalog/main.cpp
+++ book-catalog/main.cpp
@@ -2,6 +2,59 @@
 #include <limits>
 #include <string>
 
+void showMenu() {
+    std::cout << "                                     \n";
+    std::cout << "Choose the desired action            \n";
+    std::cout << "                                     \n";
+    std::cout << "1. Add book                          \n";
+    std::cout << "2. Show books                        \n";
+    std::cout << "0. Exit                              \n";
+    std::cout << "                                     \n";
+}
+
+struct Book {
+        std::string title;
+        std::string author;
+        int year;
+};
+
+void addBook (Book books[], int& books_count) {
+
+        if (books_count >= 100) {
+            std::cout << "Catalog is full! Maybe you want to view all available books?" << std::endl;
+        }
+
+        else {
+
+              std::cout << "Enter the title of the book: ";
+              std::getline(std::cin, books[books_count].title);
+
+              std::cout << "Enter the author of the book: ";
+              std::getline(std::cin, books[books_count].author);
+
+              std::cout << "Enter the year the book was published: ";
+              std::cin >> books[books_count].year;
+
+              books_count++;
+       }
+}  
+
+void showBooks (Book books[], int books_count) {
+                if (books_count == 0) {
+                    std::cout << "There are no books yet. But you can fix that =)" << std::endl;
+                }
+
+                else {
+                     for (int i = 0; i < books_count; i++) {
+                          std::cout << "       " << "\n";
+                          std::cout << "Title: " << books[i].title << "\n";
+                          std::cout << "Author: " << books[i].author << "\n";
+                          std::cout << "Year: " << books[i].year << "\n";
+                     }
+                }
+}
+
+
 int main() {
     std::cout << "                            " << std::endl;
     std::cout << "╔══════════════════════════╗" << std::endl;
@@ -11,25 +64,12 @@ int main() {
 
     int choice;
 
-    struct Book {
-        std::string title;
-        std::string author;
-        int year;
-    };
-
     Book books[100];
     int books_count = 0;
 
-
     do {
-    std::cout << "                                     \n";
-    std::cout << "Choose the desired action            \n";
-    std::cout << "                                     \n";
-    std::cout << "1. Add book                          \n";
-    std::cout << "2. Show books                        \n";
-    std::cout << "0. Exit                              \n";
-    std::cout << "                                     \n";
 
+       showMenu();
 
        std::cout << "Enter the desired action (in numbers): ";
        std::cin >> choice;
@@ -43,34 +83,14 @@ int main() {
            continue;
        }
 
+       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
     if (choice == 1) {
-
-        std::cin.ignore();
-
-        std::cout << "Enter the title of the book: ";
-        std::getline(std::cin, books[books_count].title);
-
-        std::cout << "Enter the author of the book: ";
-        std::getline(std::cin, books[books_count].author);
-
-        std::cout << "Enter the year the book was published: ";
-        std::cin >> books[books_count].year;
-
-        books_count++;
+        addBook(books, books_count);
     }
 
     else if (choice == 2) {
-             if (books_count == 0) {
-                 std::cout << "There are no books yet. But you can fix that =)" << std::endl;
-             }
-
-             else {
-                  for (int i = 0; i < books_count; i++) {
-                       std::cout << "Title: " << books[i].title << "\n";
-                       std::cout << "Author: " << books[i].author << "\n";
-                       std::cout << "Year: " << books[i].year << "\n";
-                  }
-             }
+             showBooks(books, books_count);
     }
 
     else if (choice == 0) {
@@ -85,3 +105,4 @@ int main() {
 
 return 0;
 }
+
